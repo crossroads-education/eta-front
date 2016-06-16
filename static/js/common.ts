00c3733ed470ee66b4beb2198832834654996d25
Alex Hicks <aldahick@iupui.edu>
2016-06-16 10:21:56 -0400

    Add bootstrap and es6-shim to common.ts

--- static/js/common.ts
+++ static/js/common.ts
@@ -3,6 +3,8 @@
 requirejs.config({
     "baseUrl": "/js/",
     "paths": {
+        "bootstrap": "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min",
+        "es6-shim": "https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.1/es6-shim.min",
         "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min",
         "material": "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.1.3/material.min"
     },
