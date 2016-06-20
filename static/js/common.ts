19e1d6867276aaff20a7ad6d5e1a93a6481feeae
Alex Hicks <aldahick@iupui.edu>
2016-06-20 11:06:55 -0400

    Add jquery-ui to requirejs

--- static/js/common.ts
+++ static/js/common.ts
@@ -6,6 +6,7 @@ requirejs.config({
         "bootstrap": "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min",
         "es6-shim": "https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.1/es6-shim.min",
         "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min",
+        "jquery-ui": "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min",
         "material": "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.1.3/material.min"
     },
     "shim": {
