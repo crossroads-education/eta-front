ad317bfd76fa2bacabdf8fadd70f5a417bd828ca
Alex Hicks <aldahick@iupui.edu>
2016-06-23 09:22:57 -0400

    Add chartjs to requirejs paths

--- static/js/common.ts
+++ static/js/common.ts
@@ -4,6 +4,7 @@ requirejs.config({
     "baseUrl": "/js/",
     "paths": {
         "bootstrap": "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min",
+        "chartjs": "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.min",
         "es6-shim": "https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.1/es6-shim.min",
         "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min",
         "jquery-bez": "https://cdn.jsdelivr.net/jquery.bez/1.0.11/jquery.bez.min",
