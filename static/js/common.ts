c7b20d88dd48c675a0538e8edbad6c51166eafc7
Alex Hicks <aldahick@iupui.edu>
2016-06-30 10:53:27 -0400

    Add jquery-noisy

--- static/js/common.ts
+++ static/js/common.ts
@@ -8,6 +8,7 @@ requirejs.config({
         "es6-shim": "https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.1/es6-shim.min",
         "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min",
         "jquery-bez": "https://cdn.jsdelivr.net/jquery.bez/1.0.11/jquery.bez.min",
+        "jquery-noisy": "https://cdnjs.cloudflare.com/ajax/libs/noisy/1.2/jquery.noisy.min",
         "jquery-ui": "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min"
     },
     "shim": {
