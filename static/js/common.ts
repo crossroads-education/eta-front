9fcd52394dd4478df3506c49587aedf646403fbd
Alex Hicks <aldahick@iupui.edu>
2016-07-14 14:19:49 -0400

    Add kute, kute-jquery, and kute-svg

--- static/js/common.ts
+++ static/js/common.ts
@@ -10,6 +10,9 @@ requirejs.config({
         "jquery-bez": "https://cdn.jsdelivr.net/jquery.bez/1.0.11/jquery.bez.min",
         "jquery-noisy": "https://cdnjs.cloudflare.com/ajax/libs/noisy/1.2/jquery.noisy.min",
         "jquery-ui": "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min",
+        "kute": "https://cdn.jsdelivr.net/kute.js/1.5.0/kute.min",
+        "kute-jquery": "https://cdn.jsdelivr.net/kute.js/1.5.0/kute-jquery.min",
+        "kute-svg": "https://cdn.jsdelivr.net/kute.js/1.5.0/kute-svg.min",
         "moment": "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.min"
     },
     "shim": {
