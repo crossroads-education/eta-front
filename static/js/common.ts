153833eebf6c4287fa254d48ebdd5a402d58c646
Alex Hicks <aldahick@iupui.edu>
2016-07-19 12:05:35 -0400

    Add jquery-inputmask and bootstrap-slider

--- static/js/common.ts
+++ static/js/common.ts
@@ -4,10 +4,12 @@ requirejs.config({
     "baseUrl": "/js/",
     "paths": {
         "bootstrap": "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min",
+        "bootstrap-slider": "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.1.1/bootstrap-slider.min",
         "chartjs": "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.min",
         "es6-shim": "https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.1/es6-shim.min",
         "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min",
         "jquery-bez": "https://cdn.jsdelivr.net/jquery.bez/1.0.11/jquery.bez.min",
+        "jquery-inputmask": "https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.1/jquery.inputmask.bundle.min",
         "jquery-noisy": "https://cdnjs.cloudflare.com/ajax/libs/noisy/1.2/jquery.noisy.min",
         "jquery-ui": "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min",
         "kute": "https://cdn.jsdelivr.net/kute.js/1.5.0/kute.min",
