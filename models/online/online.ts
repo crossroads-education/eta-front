68b88415f6b91d2826e478fa09eb9ddd03cf8583
Alex Hicks <aldahick@iupui.edu>
2016-11-09 13:35:42 -0500

    Add HoursOfOperation check to /online

--- models/online/online.ts
+++ models/online/online.ts
@@ -13,8 +13,17 @@ export class Model implements eta.Model {
                 OnlineRoom
                     LEFT JOIN Course ON
                         OnlineRoom.letter = Course.room
+                    LEFT JOIN HoursOfOperation ON
+                        HoursOfOperation.center = 3 AND
+                        HoursOfOperation.day = DAYOFWEEK(CURDATE()) - 1
             WHERE
-                OnlineRoom.date = CURDATE()
+                OnlineRoom.date = CURDATE() AND
+                NOT (
+                    HoursOfOperation.open = '00:00:00' AND
+                    HoursOfOperation.close = '00:00:00'
+                ) AND
+                HoursOfOperation.open <= TIME(NOW()) AND
+                HoursOfOperation.close > TIME(NOW())
             GROUP BY OnlineRoom.letter
             ORDER BY OnlineRoom.letter ASC`;
         eta.db.query(sql, [], (err: eta.DBError, rows: any[]) => {
