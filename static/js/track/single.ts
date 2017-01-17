a9a164a1bfc59120e51fef809465502db21535c2
Alex Hicks <aldahick@iupui.edu>
2017-01-17 11:42:01 -0500

    Fix professor visit tracking page

--- static/js/track/single.ts
+++ static/js/track/single.ts
@@ -1,7 +1,13 @@
 export module single {
+    function showNoVisits(): void {
+        $(".container-single p").text("This student has no recorded visits for this course.");
+        $(".container-single tbody").html("");
+        $(".container-single").removeClass("empty");
+    }
+
     $(document).ready(function() {
         let studentTable: DataTables.DataTable;
-        $(".container-professor-section,.container-athlete tr").on("click", function() {
+        $(".container-professor-section tbody tr,.container-athlete tbody tr").on("click", function() {
             let $this: JQuery = $(this);
             let studentID: string = $this.find("td[data-type='id']").text();
             let sectionID: string = $(".data[data-name='sectionID']").data("value");
@@ -12,8 +18,7 @@ export module single {
             $.post("/post/track/get-single", params, function(data) {
                 $(".container-single h1").text(data.firstName + " " + data.lastName);
                 if (data.visits.length === 0) {
-                    $(".container-single p").text("This student has no recorded visits for this course.");
-                    $(".container-single tbody").html("");
+                    showNoVisits();
                     return;
                 }
                 $(".container-single p").text("");
@@ -24,7 +29,11 @@ export module single {
                 $('html, body').animate({
                     scrollTop: $(".container-single").offset().top
                 }, 800);
-            }, "json");
+            }, "json").fail(function(err: any) {
+                if (err.statusCode === 500) {
+                    showNoVisits();
+                }
+            });
         });
         studentTable = (<any>$(".container-single table")).dataTable({
             "paging": true,
