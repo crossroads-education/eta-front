cbaee1b284ad2dca09134123a0acebb83d0c1558
Kroeker <nkroeker@iupui.edu>
2017-01-24 10:44:50 -0500

    Create login helper

--- models/login/login.ts
+++ models/login/login.ts
@@ -48,75 +48,13 @@ export class Model implements eta.Model {
                 if (eta.config.dev.sudo) {
                     username = eta.config.dev.sudo;
                 }
-                req.session["username"] = username;
-                eta.db.query("SELECT id FROM Person WHERE username = ?", [username], (err: eta.DBError, rows: any[]) => {
-                    if (err) {
-                        eta.logger.dbError(err);
-                        callback({ errcode: eta.http.InternalError });
-                        return;
+                eta.login.login(username, req, (env: { [key: string]: any }) => {
+                    if (env) {
+                        callback(env);
                     }
-                    if (rows.length == 0) {
-                        callback({ errcode: eta.http.Forbidden });
-                        return;
+                    else {
+                        eta.redirect.back(req, res);
                     }
-                    req.session["userid"] = rows[0].id;
-                    let sql: string = `
-                        SELECT
-                            Position.*,
-                            Center.department
-                        FROM
-                            EmployeePosition
-                                LEFT JOIN Position ON
-                                    EmployeePosition.position = Position.id
-                                LEFT JOIN Center ON
-                                    Position.center = Center.id
-                        WHERE
-                            EmployeePosition.id = ? AND
-                            EmployeePosition.start <= CURDATE() AND
-                            (
-                                EmployeePosition.end > CURDATE() OR
-                                ISNULL(EmployeePosition.end)
-                            )`;
-                    eta.db.query(sql, [req.session["userid"]], (err: eta.DBError, rows: any[]) => {
-                        if (err) {
-                            eta.logger.dbError(err);
-                            callback({ errcode: eta.http.InternalError });
-                            return;
-                        }
-                        req.session["department"] = rows.length > 0 ? rows[0].department : -1;
-                        req.session["positions"] = rows;
-                        sql = `SELECT
-                                student.count AS student,
-                                professor.count AS professor
-                            FROM (
-                                SELECT
-                                    COUNT(*) as count
-                                FROM
-                                    StudentSection
-                                WHERE
-                                    student = ?
-                            ) AS student, (
-                                SELECT
-                                    COUNT(*) as count
-                                FROM
-                                    Section
-                                WHERE
-                                    professor = ?
-                            ) AS professor`;
-                        eta.db.query(sql, [req.session["userid"], req.session["userid"]], (err: eta.DBError, rows: any[]) => {
-                            if (err) {
-                                eta.logger.dbError(err);
-                                callback({ errcode: eta.http.InternalError });
-                                return;
-                            }
-                            if (rows[0].professor != 0) {
-                                req.session["isProfessor"] = true;
-                            } else if (rows[0].student != 0) {
-                                req.session["isStudent"] = true;
-                            }
-                            eta.redirect.back(req, res);
-                        });
-                    });
                 });
             } else {
                 eta.logger.warn("Something is wrong with the CAS server: received response '" + response + "'.");
