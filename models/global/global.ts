c93f5e770eddbb01d5f5acf93c90b2bd77302941
Alex Hicks <aldahick@iupui.edu>
2017-02-20 11:11:22 -0500

    Implement Nexus access portal

---
+++ models/global/global.ts
@@ -0,0 +1,11 @@
+import * as eta from "eta-lib";
+
+import * as express from "express";
+
+export class Model implements eta.Model {
+    public render(req: express.Request, res: express.Response, callback: (env: { [key: string]: any }) => void): void {
+        callback({
+            "useWizIQ": eta.config.wiziq.use
+        });
+    }
+}
