32e6d78663d139d9f88f21d44f5607b1102a59af
Alex Hicks <aldahick@iupui.edu>
2017-03-08 17:16:57 -0500

    Implement subject field in contact form

--- models/post/contact/contact.ts
+++ models/post/contact/contact.ts
@@ -6,16 +6,20 @@ import * as querystring from "querystring";
 
 export class Model implements eta.Model {
     public render(req: express.Request, res: express.Response, callback: (env: { [key: string]: any }) => void): void {
-        if (!eta.params.test(req.body, ["name", "email", "phone", "message"])) {
+        if (!eta.params.test(req.body, ["name", "email", "phone", "subject", "message"])) {
             callback({ errcode: eta.http.InvalidParameters });
             return;
         }
         eta.mail.sendMail({
             "from": req.body.email,
             "to": "mathmac@iupui.edu",
-            "subject": "Comments submitted from MAC website",
-            "text": "From: " + req.body.name + "\nEmail: " + req.body.email
-            + "\nPhone: " + req.body.phone + "\nMessage:\n" + req.body.message
+            "subject": `[Website Comments] ${req.body.name}: ${req.body.subject}`,
+            "text": `From: ${req.body.name}
+                    Email: ${req.body.email}
+                    Phone: ${req.body.phone}
+                    Subject: ${req.body.subject}
+                    Message:
+                    ${req.body.message}`.replace(/    /g, "")
         }, (err: Error, info: nodemailer.SentMessageInfo) => {
             if (err) {
                 eta.logger.warn("Could not send mail from " + req.body.email + ": " + err.message);
