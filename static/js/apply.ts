0e76942e6ca8c1577a5656d10b3146e1b547d921
Alex Hicks <aldahick@iupui.edu>
2017-05-02 11:39:43 -0400

    Implement sloths on all pages

--- static/js/apply.ts
+++ static/js/apply.ts
@@ -1,6 +1,7 @@
+import "sloth";
+
 export module apply {
     $(document).ready(function() {
-        console.log('hi');
         $(".toggleButton").button();
         $(".toggleButton").on("change", function() {
             $("#" + this.getAttribute("data-name")).toggleClass("selected notSelected");
---
+++ static/js/sloth.ts
@@ -0,0 +1,20 @@
+import "jquery-sloth-konami";
+
+export module sloth {
+    function fixSlothCanvas() {
+        let slothCanvas: HTMLCanvasElement = <HTMLCanvasElement>$("canvas")[0];
+        slothCanvas.height = $("#body").height();
+    }
+
+    $(document).ready(function() {
+        $("#body").sloth({
+            "imageUrl": "/itrac/images/signin/sloth.svg",
+            "zIndex": 99999
+        });
+    });
+
+    $(window).load(function() {
+        fixSlothCanvas();
+        $(".ui-accordion").on("accordionactivate", fixSlothCanvas);
+    })
+}
