3d4aaeb463510c81f077e5e34245deb45717c816
Gaby Justice <gnjustic@iupui.edu>
2017-10-06 13:38:54 -0400

    adds online hours to Live Help page (Hopefully)

---
+++ models/nexus/nexus.ts
@@ -0,0 +1,60 @@
+import * as eta from "eta-lib";
+
+import * as express from "express";
+
+export class Model implements eta.Model {
+    public render(req: express.Request, res: express.Response, callback: (env: { [key: string]: any }) => void): void {
+        if (!req.query.term) {
+            req.query.term = eta.term.getCurrent().id;
+        }
+        let os: string = (<any>eta.http.getUserAgent(req).os).family;
+        let isMobile: boolean = false;
+        if (os == "Android" || os == "iOS") {
+            isMobile = true;
+        }
+        let sql: string = `
+            SELECT
+                floor.day as day,
+                CONCAT(CURDATE(), ' ', online.open) as onlineOpen,
+                CONCAT(CURDATE(), ' ', online.close) as onlineClose
+            FROM (
+                SELECT
+                    day,
+                    open,
+                    close
+                FROM
+                    HoursOfOperation
+                WHERE
+                    term = ? AND
+                    center = 2) AS floor
+                        LEFT JOIN (
+                            SELECT
+                                day,
+                                open,
+                                close
+                            FROM
+                                HoursOfOperation
+                            WHERE
+                                term = ? AND
+                                center = 3) AS online
+                            ON floor.day = online.day`;
+        eta.db.query(sql, [req.query.term, req.query.term], (err: eta.DBError, rows: any[]) => {
+            if (err) {
+                eta.logger.dbError(err);
+                callback({ errcode: eta.http.InternalError });
+                return;
+            }
+            let fieldNames: string[] = ["onlineOpen", "onlineClose"];
+            for (let i: number = 0; i < rows.length; i++) {
+                rows[i].day = eta.time.shortDaysOfWeek[i];
+                for (let j: number = 0; j < fieldNames.length; j++) {
+                    rows[i][fieldNames[j]] = eta.time.getShortTime(new Date(rows[i][fieldNames[j]]));
+                }
+            }
+            callback({
+                rows: rows,
+                isMobile: isMobile
+            });
+        })
+    }
+}
